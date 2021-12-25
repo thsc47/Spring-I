a6f95c4041cfdc200483921a078a7df12c372292
thsc47 <thsc47@gmail.com>
2021-12-25 17:16:38 -0300

    chore: sync local repo

---
+++ src/main/java/com/spring/springi/SpringIApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/springi/SpringIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
